6efc71747f6a887d254b92073a27d4af533125a9
duongcongke <103102773+duongcongke@users.noreply.github.com>
2022-07-18 17:32:52 +0700

    up

---
+++ ConsoleApp3/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            int k = 10;
+            int m = 20;
+            int s = k + m;
+            bool t = m > k;
+            double z = Math.Sqrt(5);
+            const double pi = 3.14;
+            Console.WriteLine("The sum is: " + s);
+            Console.WriteLine("Is m > k ?" + t);
+            Console.WriteLine("The sqrt of 5: " + z);
+            Console.WriteLine("The max value of Int32: " + Int32.MaxValue);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp3/Class4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company
+{
+    class Product
+    {
+        static void Main(string[] args)
+        {
+            Object objProductID;
+            Object objProductName;
+            Object obijPrice;
+            Object objQuantity;
+            Console.WriteLine("Enter the name of product : ");
+            objProductID = Convert.ToInt32(Con)
+
+
+        }
+    }
+    internal class Class4
+    {
+    }
+}
